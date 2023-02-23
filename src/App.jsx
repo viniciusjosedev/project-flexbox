12aa5d07fe9a4d1c98049d80d52658127e876b21
Vinicius José <viniciusjosedev@gmail.com>
2023-02-23 09:15:41 -0300

    project started

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>teste</h1>
+      </header>
+      <main>
+        <h1>teste2</h1>
+      </main>
+      <footer>
+        <h1>teste3</h1>
+      </footer>
+    </>
+  );
+}
+
+export default App;
